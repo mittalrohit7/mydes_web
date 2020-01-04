f75ca28bebdfeaa51ff97d5500958bafd5e9223e
Rohit Mittal <mittalrohit7@gmail.com>
2020-01-05 00:01:50 +0530

    Add files via upload

---
+++ src/main/java/com/mydes/springboot/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.mydes.springboot.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mydes.springboot.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByName(String name);
+
+}
