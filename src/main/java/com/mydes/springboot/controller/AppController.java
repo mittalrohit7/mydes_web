4b32d4bd77acbf613b73aef0821279afd9c6228c
Rohit Mittal <mittalrohit7@gmail.com>
2020-01-05 14:29:55 +0530

    Logging

--- src/main/java/com/mydes/springboot/controller/AppController.java
+++ src/main/java/com/mydes/springboot/controller/AppController.java
@@ -1,5 +1,7 @@
 package com.mydes.springboot.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,15 +9,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AppController {
+	public static final Logger logger = LoggerFactory.getLogger(AppController.class);
 
 	@RequestMapping("/")
 	String home(ModelMap modal) {
+		logger.info("Fetching User with home");
 		modal.addAttribute("title","CRUD Example");
 		return "index";
 	}
 
 	@RequestMapping("/partials/{page}")
 	String partialHandler(@PathVariable("page") final String page) {
+		logger.info("Fetching User with partialHandler");
 		return page;
 	}
 
